a2a8bb75560110ccb627aac0bd0e45355e11830a
Marc Donald <dev@marcdonald.com>
2023-03-26 20:25:29 +0100

    chore: update dependencies

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,7 +1,1 @@
-module.exports = {
-  '*.{js,ts}': [
-    'eslint --fix -c .eslintrc.js --ext',
-    'prettier -c .prettierrc.js --write',
-  ],
-  '*.{yml,yaml,md,json}': ['prettier -c .prettierrc.js --write'],
-};
+module.exports = require('@marcdonald/lint-staged-config');
